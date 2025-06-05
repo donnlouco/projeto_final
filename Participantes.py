49664ff52d44fe9402aeffe0504ef5c2ddb6468e
donnlouco <vinicius.lima6@estudante.ifms.edu.br>
2025-06-05 00:58:01 -0300

    comecando algo

--- Participantes.py
+++ Participantes.py
@@ -4,4 +4,16 @@ from Util import *
 Visitantes = {}
 
 def Participantes():
-    pass
+    while True:
+        nome = input('Digite o NOME COMPLETO do participante: ')
+        cpf = input('Digite o CPF do participante: ')
+        email = input('Digite o E-MAIL do participante: ')
+        
+        Visitantes[cpf] = {'nome': nome, 'email': email}
+        print(f'Os dados foram aceitos ')        
+        if nome == '0':
+            break
+    lista_participantes = [x for x in Visitantes.items()]
+    print(lista_participantes)
+
+Participantes()        
