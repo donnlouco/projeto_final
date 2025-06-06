cbb275cb93fab27ceaccdf447136b3f77ca0b152
donnlouco <vinicius.lima6@estudante.ifms.edu.br>
2025-06-05 21:09:35 -0300

    fiz os participantes

--- Eventos.py
+++ Eventos.py
@@ -1,4 +1,5 @@
 from Util import *
+from Participantes import *
 
 
 def Eventos():
--- Participantes.py
+++ Participantes.py
@@ -3,18 +3,21 @@ from Util import *
 
 Visitantes = {}
 
-def Participantes():
+def participantes():
     while True:
         nome = input('Digite o NOME COMPLETO do participante: ')
         cpf = input('Digite o CPF do participante: ')
         email = input('Digite o E-MAIL do participante: ')
-        
-        Visitantes[cpf] = {'nome': nome, 'email': email}
-        print(f'Os dados foram aceitos ')        
-        if nome == '0':
+        preferencias = input('Digite os temas preferidos: ')
+        Visitantes[cpf] = {'Nome': nome, 'Email': email, 'Preferencias Tematicas': preferencias}
+        print(f'Os dados foram aceitos ')
+        saida = input('Digite 0 se deseja parar de cadastrar: ')       
+        if saida == '0':
             break
-    lista_participantes = [x for x in Visitantes.items()]
-    print(lista_participantes)
+        
+def listar_participantes():
+    for lista in Visitantes.items():
+        print(lista)
 
-Participantes()        
+participantes()        
 
